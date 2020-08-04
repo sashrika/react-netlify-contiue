ab20b992d1aaa12ea4c9b406f93d8ffd3d426742
sashrika <sashrika@liveroom.xyz>
2020-08-04 13:24:49 +0530

    from git

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. linked both! now with git
         </a>
       </header>
     </div>
